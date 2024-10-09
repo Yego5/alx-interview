309268c0821c97f13ccc5960d0ab2fc9cad9598b
yego5 <shachz.github@gmail.com>
2024-10-08 17:43:37 -0700

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,48 +1,27 @@
 #!/usr/bin/python3
-'''module: 5-island_perimeter
- returns the perimeter of the island based on `grid`
- grid is a list of list of integers:
-    0 represents a water zone
-    1 represents a land zone
-    One cell is a square with side length 1
-    Grid cells are connected horizontally/vertically (not diagonally).
-    Grid is rectangular, width and height don’t exceed 100
-Grid is completely surrounded by water, and there is one island (or nothing).
-The island doesn’t have “lakes”
-    (water inside that isn’t connected to the water around the island).
-'''
+"""Island Perimeter Problem
+"""
 
 
 def island_perimeter(grid):
-    '''
-    computes the perimeter of the island described in the grid.
+    """
+    Calculates the perimeter of the island described in grid
+    Args:
+        grid: 2d list of integers containing 0(water) or 1(land)
+    Return:
+        the perimeter of the island
+    """
 
-    args:
-        grid: list of list of integers representing the island.
-
-    returns:
-        the perimeter of the island.
-
-    '''
-    perimeter = 0
-
-    # check for cell in grid
+    p = 0
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                # check if the cell has water on any of its four sides
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-
-    return perimeter
-
-
-if __name__ == '__main__':
-    print(island_perimeter(grid))
-
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return 
